3d4da3a4c7bb2c74c185519eca7fece27dc2dd81
Funduluka Shangala <53972016+fshangala@users.noreply.github.com>
2021-04-20 13:49:08 +0200

    Update database-connection.php

--- database-connection.php
+++ database-connection.php
@@ -1,23 +1,27 @@
 <?php
 
-class Db{
+abstract class Db{
 
 	public $servername = "localhost";
 	public $username = "root";
 	public $password = "";
-	public $dbname = "exsae_chat";
+	public $dbname = "mumarket";
 	public $conn;
 	public $error;
+	public $table;
+	public $fields = ["id"=>0];
 
 	function __construct(){
 		$this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
 		if ($this->conn->connect_error) {
 			$this->error = $this->conn->connect_error;
 		}
+		$this->__init__();
 	}
 
-	function get_item_by_id($table_name, $item_id){
-		$sql = "SELECT * FROM ".$table_name." WHERE id=".$item_id;
+	abstract function __init__();
+
+	function return_one($sql){
 		$result = $this->conn->query($sql);
 		if ($result){
 			$data = $result->fetch_assoc();
@@ -28,14 +32,18 @@ class Db{
 		}
 	}
 
-	function get_all_items($table_name){
-		$sql = "SELECT * FROM ".$table_name;
+	function get($item_id){
+		$sql = "SELECT * FROM ".$this->table." WHERE id=".$item_id;
+		return $this->return_one($sql);
+	}
+
+	function return_many($sql){
 		$result = $this->conn->query($sql);
 		if($result){
 			$data = $result->fetch_all();
 			$r = array();
 			foreach($data as $datum){
-				array_push($r, $this->get_item_by_id($table_name, $datum[0]));
+				array_push($r, $this->get($datum[0]));
 			}
 			return $r;
 		} else {
@@ -45,18 +53,11 @@ class Db{
 	}
 
 	function get_all(){
-		$data = $this->get_all_items($this->table_name);
-		return $data;
+		$sql = "SELECT * FROM ".$this->table;
+		return $this->return_many($sql);
 	}
 
-	function post_item($table_name, $data){
-		$sql = "INSERT INTO ".$table_name." (id";
-		$sql2 = ") VALUES ('0'";
-		foreach($data as $key=>$value){
-			$sql .= ",".$key;
-			$sql2 .= ",'".$value."'";
-		}
-		$sql .= ");";
+	function post($sql){
 		$result = $this->conn->query($sql);
 		if ($result != TRUE){
 			$this->error = $this->conn->error;
@@ -64,8 +65,20 @@ class Db{
 		return $result;
 	}
 
-	function update_item_by_id($table_name, $id, $data){
-		$sql = "UPDATE ".$table_name." SET ";
+	function post_item($data){
+		$sql = "INSERT INTO ".$this->table." (id";
+		$sql2 = ") VALUES ('0'";
+		foreach($data as $key=>$value){
+			$sql .= ",".$key;
+			$sql2 .= ",'".$value."'";
+		}
+		$sql2 .= ");";
+		$sql .= $sql2;
+		return $this->post($sql);
+	}
+
+	function update_item_by_id($id, $data){
+		$sql = "UPDATE ".$this->table." SET ";
 		$i = 0;
 		foreach($data as $key=>$value){
 			if ($i == 0){
@@ -76,19 +89,12 @@ class Db{
 			$i++;
 		}
 		$sql .= " WHERE id=$id;";
-		$result = $this->conn->query($sql);
-		if ($result != TRUE){
-			$this->error = $this->conn->error;
-		}
-		return $sql;
+		return $this->post($sql);
 	}
 
-	function delete_item_by_id($table_name, $id){
-		$sql = "DELETE FROM $table_name WHERE id=$id";
-		$result = $this->conn->query($sql);
-		if ($result != TRUE){
-			$this->error = $this->conn->error;
-		}
+	function delete_item_by_id($id){
+		$sql = "DELETE FROM $this->table WHERE id=$id";
+		return $this->post($sql);
 	}
 
 }
